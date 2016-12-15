fb2cec84fe5d9d68041c1bb7eded20df932d3ee5
Thomas Hoen <tom@givecorps.om>
2016-12-15 18:09:36 -0500

    properly name the serializer

--- app/serializers/nfg_jwt_token_security/v1/admin_serializer.rb
+++ app/serializers/nfg_jwt_token_security/v1/admin_serializer.rb
@@ -1,4 +1,4 @@
-module GivecorpsApi::V1
+module NfgJwtTokenSecurity::V1
   class AdminSerializer < ActiveModel::Serializer
     attributes :id, :first_name, :last_name, :email, :status, :roles
   end
